2d0c17e4d37b0af62a32d916d858822c059a2108
Jeremy <unknown>
2013-02-08 00:58:46 -0700

    First work on sort program

--- sort.lua
+++ sort.lua
@@ -6,11 +6,36 @@ SOUTH = 3
 WEST = 4
 EAST = 5
 
+deMungeId = function(uuid)
+  local subt = bit.band(uuid, 0x7fff)
+  local dexorm = bit.bxor(subt, 0x3a69)
+  local metadata = nil
+  if dexorm ~= 28262 then -- item takes dmg
+    metadata = bit.bxor(dexorm, 0x6e6c)
+  end
+  local id = bit.bxor((uuid-subt)/0x8000, 0x4f89)
+  return id, metadata
+end
+
+local directions = { [0]=0,[1]=1,[2]=2,[3]=3,[4]=4,[5]=5,["down"] = 0, ["up"] = 1, ["-Z"] = 2, ["+Z"] = 3, ["-X"] = 4, ["+X"] = 5, ["+Y"] = 1, ["-Y"] = 0}
+directions.south = directions["+Z"]
+directions.east = directions["+X"]
+directions.north = directions["-Z"]
+directions.west = directions["-X"]
+
+
 print('sorting...')
 s = peripheral.wrap('right')
--- for k,v in pairs(s) do
---   print(k)
--- end
-for k,v in pairs(s.list(SOUTH)) do
-  print(k..', '..v)
+
+--s.sort(directions.north, 1)
+uuid, count = os.pullEvent('isort_item')
+s.sort(directions.north, 32)
+if count > 32 then
+  sleep(1)
+  s.sort(directions.north, 32)
 end
+
+-- for k,v in pairs(s.list(SOUTH)) do
+--   id, meta = deMungeId(k)
+--   print('id: '..id..', meta: '..meta..', count: '..v)
+-- end
