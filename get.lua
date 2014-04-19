ac8da11860ba24db31d811583715d6b2f295a288
Jeremy Mickelson <Jeremy.Mickelson@gmail.com>
2014-04-19 11:29:12 -0600

    Applied the same improvements to get and run

--- get.lua
+++ get.lua
@@ -4,7 +4,10 @@ name = args[1]
 print('Downloading '..name..' ...')
 
 local f, h
-f = http.get('http://jrm.homenet.org/ComputerCraft/'..name..'.lua.txt')
+f = http.get('http://jrm.homenet.org/ComputerCraft/'..name..'.lua')
+if not f then
+  error('Could not download file '..name..'!')
+end
 h = fs.open(shell.resolve(name), 'w')
 h.write(f.readAll())
 h.close()
--- run.lua
+++ run.lua
@@ -1,5 +1,8 @@
 args = { ... }
 name = args[1]
 print('running '..name..' ...')
-f = http.get('http://jrm.homenet.org/ComputerCraft/'..name..'.lua.txt')
+f = http.get('http://jrm.homenet.org/ComputerCraft/'..name..'.lua')
+if not f then
+  error('Could not download file '..name..'!')
+end
 loadstring(f.readAll())()
